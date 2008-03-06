2a12a0568c993b2de07263fba7f2b070515bd205
Anton <antonglv@gmail.com>
2008-03-06 15:40:47 +0000

    * Just debug definitions added

--- components/CBKeyMapComponent.js
+++ components/CBKeyMapComponent.js
@@ -18,6 +18,20 @@
 // Author: Anton Glazatov (c) 2008
 // 
 // ***** END LICENSE BLOCK *****
+    function dLOG (text)
+    {
+          var consoleService = Components. classes ["@mozilla.org/consoleservice;1"]. getService (Components. interfaces. nsIConsoleService);
+          consoleService. logStringMessage (text);
+    }
+    function dEXTLOG (aMessage, aSourceName, aSourceLine, aLineNumber,
+              aColumnNumber, aFlags, aCategory)
+    {
+      var consoleService = Components. classes ["@mozilla.org/consoleservice;1"]. getService (Components. interfaces. nsIConsoleService);
+      var scriptError = Components. classes ["@mozilla.org/scripterror;1"]. createInstance (Components. interfaces. nsIScriptError);
+      scriptError. init (aMessage, aSourceName, aSourceLine, aLineNumber,
+                 aColumnNumber, aFlags, aCategory);
+      consoleService. logMessage (scriptError);
+    }
 function cbKeyMapService () {}
 cbKeyMapService. prototype =
 {
