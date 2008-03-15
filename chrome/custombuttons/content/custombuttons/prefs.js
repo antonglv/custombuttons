41facbfa824d535043a832dbf3779082e4db4571
Anton <antonglv@gmail.com>
2008-03-15 04:31:09 +0000

    + Added: global oncommand handler to handle extra checkboxes attributes

--- chrome/custombuttons/content/custombuttons/prefs.js
+++ chrome/custombuttons/content/custombuttons/prefs.js
@@ -15,102 +15,127 @@
 function Prefs () {}
 Prefs. prototype =
 {
-  _ps: null,
-  get ps ()
+ _ps: null,
+ get ps ()
+ {
+  if (!this. _ps)
   {
-    if (!this. _ps)
-    {
-      this. _ps = Components. classes ["@mozilla.org/preferences-service;1"]. getService (Components. interfaces. nsIPrefService);
-      this. _ps = this. _ps. QueryInterface (Components. interfaces. nsIPrefBranch);
-    }
-    return this. _ps;
-  },
+   this. _ps = Components. classes ["@mozilla.org/preferences-service;1"]. getService (Components. interfaces. nsIPrefService);
+   this. _ps = this. _ps. QueryInterface (Components. interfaces. nsIPrefBranch);
+  }
+  return this. _ps;
+ },
 
-  handleCheckboxes: function (mode)
+ handleCheckboxes: function (mode)
+ {
+  var setCheckboxesFlag = (mode || (mode == 0));
+  var cbks = document. getElementsByTagName ("checkbox");
+  var mask, num, result = 0;
+  for (var i = 0; i < cbks. length; i++)
   {
-    var setCheckboxesFlag = (mode || (mode == 0));
-    var cbks = document. getElementsByTagName ("checkbox");
-    var mask, num, result = 0;
-    for (var i = 0; i < cbks. length; i++)
-    {
-      num = cbks [i]. id. match (/modebit(\d+)$/);
-      if (!num)
-        continue;
-      mask = 1 << num [1];
-      if (setCheckboxesFlag)
-        cbks [i]. checked = ((mode & mask) == mask);
-      else
-        result |= cbks [i]. checked? mask: 0;
-    }
-    return result;
-  },
+   num = cbks [i]. id. match (/modebit(\d+)$/);
+   if (!num)
+    continue;
+   mask = 1 << num [1];
+   if (setCheckboxesFlag)
+    cbks [i]. checked = ((mode & mask) == mask);
+   else
+    result |= cbks [i]. checked? mask: 0;
+  }
+  return result;
+ },
 
-    onLoad: function ()
-    {
-    var cbps = this. ps. getBranch ("custombuttons.");
-    var mode = cbps. getIntPref ("mode");
-    this. handleCheckboxes (mode);
-        return true;
-    },
+ onLoad: function ()
+ {
+  var cbps = this. ps. getBranch ("custombuttons.");
+  var mode = cbps. getIntPref ("mode");
+  this. handleCheckboxes (mode);
+  var oFormatSelector = document. getElementById ("modebit3");
+  oFormatSelector. disabled = mode & 1;
+  window. addEventListener ("command", this, false);
+ },
 
-    onAccept: function ()
-    {
-        var mode = this. handleCheckboxes (null);
-    var cbps = this. ps. getBranch ("custombuttons.");
-    cbps. setIntPref ("mode", mode);
-        return true;
-    },
+ onAccept: function ()
+ {
+  window. removeEventListener ("command", this, false);
+  var mode = this. handleCheckboxes (null);
+  var cbps = this. ps. getBranch ("custombuttons.");
+  cbps. setIntPref ("mode", mode);
+  return true;
+ },
 
-    onCancel: function ()
-    {
-        return true;
-    }
+ onCancel: function ()
+ {
+  window. removeEventListener ("command", this, false);
+  return true;
+ },
+
+ onCommand: function (oEvent)
+ {
+  var oTarget = oEvent. target;
+  if (oTarget. nodeName == "checkbox")
+  {
+   if (oTarget. hasAttribute ("cbblocks"))
+   {
+    var sBlockedElementId = oTarget. getAttribute ("cbblocks");
+    var oBlockedElement = document. getElementById (sBlockedElementId);
+    oBlockedElement. disabled = oTarget. checked;
+   }
+  }
+ },
+
+ // EventListener interface
+ handleEvent: function (oEvent)
+ {
+  if (oEvent. type == "command")
+   this. onCommand (oEvent);
+ }
 };
 
 function TBPrefs () {}
 TBPrefs. prototype =
 {
-  pn: "network.protocol-handler.expose.custombutton",
+ pn: "network.protocol-handler.expose.custombutton",
 
-  _checkbox: null,
-  get checkbox ()
-  {
-    if (!this. _checkbox)
-      this. _checkbox = document. getElementById ("cbEnableCBProtocol");
-    return this. _checkbox;
-  },
+ _checkbox: null,
+ get checkbox ()
+ {
+  if (!this. _checkbox)
+   this. _checkbox = document. getElementById ("cbEnableCBProtocol");
+  return this. _checkbox;
+ },
 
-  onLoad: function ()
-  {
-    this. __super. prototype. onLoad. apply (this, []);
-    var state = this. ps. prefHasUserValue (this. pn) &&
-          this. ps. getBoolPref (this. pn);
-    this. checkbox. setAttribute ("checked", state);
-    this. checkbox. removeAttribute ("hidden"); // checkbox visible only in Thunderbird
-    return true;
-  },
+ onLoad: function ()
+ {
+  this. __super. prototype. onLoad. apply (this, []);
+  var state = this. ps. prefHasUserValue (this. pn) &&
+     this. ps. getBoolPref (this. pn);
+  this. checkbox. setAttribute ("checked", state);
+  this. checkbox. removeAttribute ("hidden"); // checkbox visible only in Thunderbird
+  return true;
+ },
 
-  onAccept: function ()
+ onAccept: function ()
+ {
+  this. __super. prototype. onAccept. apply (this, []);
+  if (this. checkbox. hasAttribute ("checked") &&
+   (this. checkbox. getAttribute ("checked") == "true"))
   {
-    this. __super. prototype. onAccept. apply (this, []);
-    if (this. checkbox. hasAttribute ("checked") &&
-      (this. checkbox. getAttribute ("checked") == "true"))
-    {
-      this. ps. setBoolPref (this. pn, true);
-    }
-    else if (this. ps. prefHasUserValue (this. pn))
-    {
-      try
-      {
-        this. ps. deleteBranch (this. pn);
-      }
-      catch (e)
-      {
-        this. ps. setBoolPref (this. pn, false);
-      }
-    }
-    return true;
+   this. ps. setBoolPref (this. pn, true);
   }
+  else if (this. ps. prefHasUserValue (this. pn))
+  {
+   try
+   {
+    this. ps. deleteBranch (this. pn);
+   }
+   catch (e)
+   {
+    this. ps. setBoolPref (this. pn, false);
+   }
+  }
+  return true;
+ }
 };
 TBPrefs. prototype. __proto__ = Prefs. prototype; TBPrefs. prototype. __super = Prefs;
 
--- chrome/custombuttons/content/custombuttons/prefs.sjs
+++ chrome/custombuttons/content/custombuttons/prefs.sjs
@@ -3,102 +3,127 @@
 function Prefs () {}
 Prefs. prototype =
 {
-  _ps: null,
-  get ps ()
-  {
-    if (!this. _ps)
-    {
-      this. _ps = SERVICE (PREF);
-      this. _ps = this. _ps. QI (nsIPrefBranch);
-    }
-    return this. _ps;
-  },
+	_ps: null,
+	get ps ()
+	{
+		if (!this. _ps)
+		{
+			this. _ps = SERVICE (PREF);
+			this. _ps = this. _ps. QI (nsIPrefBranch);
+		}
+		return this. _ps;
+	},
 
-  handleCheckboxes: function (mode)
-  {
-    var setCheckboxesFlag = (mode || (mode == 0));
-    var cbks = document. getElementsByTagName ("checkbox");
-    var mask, num, result = 0;
-    for (var i = 0; i < cbks. length; i++)
-    {
-      num = cbks [i]. id. match (/modebit(\d+)$/);
-      if (!num)
-        continue;
-      mask = 1 << num [1];
-      if (setCheckboxesFlag)
-        cbks [i]. checked = ((mode & mask) == mask);
-      else
-        result |= cbks [i]. checked? mask: 0;
-    }
-    return result;
-  },
+	handleCheckboxes: function (mode)
+	{
+		var setCheckboxesFlag = (mode || (mode == 0));
+		var cbks = document. getElementsByTagName ("checkbox");
+		var mask, num, result = 0;
+		for (var i = 0; i < cbks. length; i++)
+		{
+			num = cbks [i]. id. match (/modebit(\d+)$/);
+			if (!num)
+				continue;
+			mask = 1 << num [1];
+			if (setCheckboxesFlag)
+				cbks [i]. checked = ((mode & mask) == mask);
+			else
+				result |= cbks [i]. checked? mask: 0;
+		}
+		return result;
+	},
 
-    onLoad: function ()
-    {
-    var cbps = this. ps. getBranch ("custombuttons.");
-    var mode = cbps. getIntPref ("mode");
-    this. handleCheckboxes (mode);
-        return true;
-    },
+	onLoad: function ()
+	{
+		var cbps = this. ps. getBranch ("custombuttons.");
+		var mode = cbps. getIntPref ("mode");
+		this. handleCheckboxes (mode);
+		var oFormatSelector = ELEMENT ("modebit3");
+		oFormatSelector. disabled = mode & CB_MODE_USE_XML_BUTTON_FORMAT;
+		window. addEventListener ("command", this, false);
+	},
 
-    onAccept: function ()
-    {
-        var mode = this. handleCheckboxes (null);
-    var cbps = this. ps. getBranch ("custombuttons.");
-    cbps. setIntPref ("mode", mode);
-        return true;
-    },
+	onAccept: function ()
+	{
+		window. removeEventListener ("command", this, false);
+		var mode = this. handleCheckboxes (null);
+		var cbps = this. ps. getBranch ("custombuttons.");
+		cbps. setIntPref ("mode", mode);
+		return true;
+	},
 
-    onCancel: function ()
-    {
-        return true;
-    }
+	onCancel: function ()
+	{
+		window. removeEventListener ("command", this, false);
+		return true;
+	},
+	
+	onCommand: function (oEvent)
+	{
+		var oTarget = oEvent. target;
+		if (oTarget. nodeName == "checkbox")
+		{
+			if (oTarget. hasAttribute ("cbblocks"))
+			{
+				var sBlockedElementId = oTarget. getAttribute ("cbblocks");
+				var oBlockedElement = ELEMENT (sBlockedElementId);
+				oBlockedElement. disabled = oTarget. checked;
+			}
+		}
+	},
+	
+	// EventListener interface
+	handleEvent: function (oEvent)
+	{
+		if (oEvent. type == "command")
+			this. onCommand (oEvent);
+	}
 };
 
 function TBPrefs () {}
 TBPrefs. prototype =
 {
-  pn: "network.protocol-handler.expose.custombutton",
-
-  _checkbox: null,
-  get checkbox ()
-  {
-    if (!this. _checkbox)
-      this. _checkbox = ELEMENT ("cbEnableCBProtocol");
-    return this. _checkbox;
-  },
-
-  onLoad: function ()
-  {
-    SUPER (onLoad);
-    var state = this. ps. prefHasUserValue (this. pn) &&
-          this. ps. getBoolPref (this. pn);
-    this. checkbox. setAttribute ("checked", state);
-    this. checkbox. removeAttribute ("hidden"); // checkbox visible only in Thunderbird
-    return true;
-  },
-
-  onAccept: function ()
-  {
-    SUPER (onAccept);
-    if (this. checkbox. hasAttribute ("checked") &&
-      (this. checkbox. getAttribute ("checked") == "true"))
-    {
-      this. ps. setBoolPref (this. pn, true);
-    }
-    else if (this. ps. prefHasUserValue (this. pn))
-    {
-      try
-      {
-        this. ps. deleteBranch (this. pn);
-      }
-      catch (e)
-      {
-        this. ps. setBoolPref (this. pn, false);
-      }
-    }
-    return true;
-  }
+	pn: "network.protocol-handler.expose.custombutton",
+	
+	_checkbox: null,
+	get checkbox ()
+	{
+		if (!this. _checkbox)
+			this. _checkbox = ELEMENT ("cbEnableCBProtocol");
+		return this. _checkbox;
+	},
+	
+	onLoad: function ()
+	{
+		SUPER (onLoad);
+		var state = this. ps. prefHasUserValue (this. pn) &&
+					this. ps. getBoolPref (this. pn);
+		this. checkbox. setAttribute ("checked", state);
+		this. checkbox. removeAttribute ("hidden"); // checkbox visible only in Thunderbird
+		return true;
+	},
+	
+	onAccept: function ()
+	{
+		SUPER (onAccept);
+		if (this. checkbox. hasAttribute ("checked") &&
+			(this. checkbox. getAttribute ("checked") == "true"))
+		{
+			this. ps. setBoolPref (this. pn, true);
+		}
+		else if (this. ps. prefHasUserValue (this. pn))
+		{
+			try
+			{
+				this. ps. deleteBranch (this. pn);
+			}
+			catch (e)
+			{
+				this. ps. setBoolPref (this. pn, false);
+			}
+		}
+		return true;
+	}
 };
 EXTENDS (TBPrefs, Prefs);
 
