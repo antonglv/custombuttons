b6aa5b32c858481b2610af3a8c0cac2344abc74a
Anton <antonglv@gmail.com>
2008-10-11 17:27:32 +0000

    # Fixed: cb2 api support bugs

--- chrome/custombuttons/content/custombuttons/overlay.js
+++ chrome/custombuttons/content/custombuttons/overlay.js
@@ -605,11 +605,7 @@ Custombuttons. prototype =
    this. setButtonParameters (null, button. parameters, false);
   else if (res == 2) // Edit... pressed
    this. editButtonLink (button);
-  /* --- if (confirm (str))
-
-			this. setButtonParameters (null, button. parameters, false);
-
-		return true; */
+  return true;
  },
 
  execute_oncommand_code: function (code, button)
@@ -985,6 +981,16 @@ custombuttons.uChelpButton = function ( oBtn ) //{{{
 // Custombuttons utils
 const custombuttonsUtils =
 {
+ addMethodGate: function (srcObject, sMethodName, dstObject)
+ {
+  dstObject [sMethodName] = function ()
+  {
+   return function ()
+   {
+    return srcObject [sMethodName]. apply (srcObject, arguments);
+   }
+  } ();
+ },
 
     /**  createMsg( [title] )
 
@@ -1558,12 +1564,27 @@ gClipboard: { //{{{
 
 const createMsg = custombuttonsUtils. createMsg;
 const gClipboard = custombuttonsUtils. gClipboard;
+/*
+
 custombuttons. isPref = custombuttonsUtils. isPref;
+
 custombuttons. getPrefs = custombuttonsUtils. getPrefs;
+
 custombuttons. setPrefs = custombuttonsUtils. setPrefs;
+
 custombuttons. clearPrefs = custombuttonsUtils. clearPrefs;
+
 custombuttons. readFile = custombuttonsUtils. readFile;
+
 custombuttons. writeFile = custombuttonsUtils. writeFile;
+
+*/
+custombuttonsUtils. addMethodGate (custombuttonsUtils, "isPref", custombuttons);
+custombuttonsUtils. addMethodGate (custombuttonsUtils, "getPrefs", custombuttons);
+custombuttonsUtils. addMethodGate (custombuttonsUtils, "setPrefs", custombuttons);
+custombuttonsUtils. addMethodGate (custombuttonsUtils, "clearPrefs", custombuttons);
+custombuttonsUtils. addMethodGate (custombuttonsUtils, "readFile", custombuttons);
+custombuttonsUtils. addMethodGate (custombuttonsUtils, "writeFile", custombuttons);
 
 window. addEventListener ("load", custombuttons, false);
 window. addEventListener ("unload", custombuttons, false);
