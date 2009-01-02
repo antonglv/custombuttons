5174bef9f38f3178f1450dad84aff2cfe071dc0d
Anton <antonglv@gmail.com>
2009-01-02 02:47:24 +0000

    no message

--- components/CBKeyMapComponent.js
+++ components/CBKeyMapComponent.js
@@ -1,22 +1,22 @@
 // ***** BEGIN LICENSE BLOCK *****
 // Version: MPL 1.1
-// 
+//
 // The contents of this file are subject to the Mozilla Public License Version
 // 1.1 (the "License"); you may not use this file except in compliance with
 // the License. You may obtain a copy of the License at
 // http://www.mozilla.org/MPL/
-// 
+//
 // Software distributed under the License is distributed on an "AS IS" basis,
 // WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
 // for the specific language governing rights and limitations under the
 // License.
-// 
+//
 // Custom Buttons:
 // - Gives a possibility to create custom toolbarbuttons.
 // - This component is intended to support custombuttons "hot keys" definition
-// 
+//
 // Author: Anton Glazatov (c) 2008
-// 
+//
 // ***** END LICENSE BLOCK *****
 function cbKeyMapService () {}
 cbKeyMapService. prototype =
