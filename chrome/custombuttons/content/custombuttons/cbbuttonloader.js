c5a5a61e775c75e85804d772896c3573ac16b845
antonglv <none@none>
2010-06-27 19:47:19 +0700

    + Added: cbbuttonloader.js to load 'custombutton' object only once

---
+++ chrome/custombuttons/content/custombuttons/cbbuttonloader.js
@@ -0,0 +1,4 @@
+
+
+if (!("custombutton" in window))
+    Components. classes ["@mozilla.org/moz/jssubscript-loader;1"]. getService (Components. interfaces. mozIJSSubScriptLoader). loadSubScript ("chrome://custombuttons/content/cbbutton.js");
