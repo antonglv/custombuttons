1a92d74287f84d17296272d46e1149bb2b6ec60e
antonglv <none@none>
2010-08-17 22:25:43 +0700

    Now backups will save to "backup" subdirectory

--- components/CustomButtonsService.js
+++ components/CustomButtonsService.js
@@ -33,6 +33,22 @@ function backupProfile (phase)
     var profileDir = Components. classes ["@mozilla.org/file/directory_service;1"]. getService (Components. interfaces. nsIProperties). get ("ProfD", Components. interfaces. nsIFile);
     profileDir. append ("custombuttons");
     var backupDir = profileDir. clone ();
+    backupDir. append ("backup");
+    if (!backupDir. exists ())
+    {
+ try
+ {
+     backupDir. create (1, 0755);
+ }
+ catch (e)
+ {
+     var msg = 'Custom Buttons error.]' +
+  '[Event: Creating custombuttons backup directory]' +
+  '[ ' + e;
+     Components. utils. reportError (msg);
+     return;
+ }
+    }
     var num = 3;
     var makeFlag = true;
     switch (phase)
