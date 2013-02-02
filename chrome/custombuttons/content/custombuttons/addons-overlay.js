c63cfa946a75a9d4147810190121d92f95dff937
antonglv <none@none>
2013-02-02 23:12:19 +0700

    Reindent code
    --HG--
    branch : bug24045

--- chrome/custombuttons/content/custombuttons/addons-overlay.js
+++ chrome/custombuttons/content/custombuttons/addons-overlay.js
@@ -2,46 +2,44 @@
  *
  */
 (function () {
-Components. utils. import ("resource://gre/modules/AddonManager.jsm");
-Components. utils. import ("resource://custombuttons-modules/addons4.js");
+     Components. utils. import ("resource://gre/modules/AddonManager.jsm");
+     Components. utils. import ("resource://custombuttons-modules/addons4.js");
 
-function init (aEvent)
-{
-    gViewController. commands. cmd_custombuttons_edit = {
-	isEnabled: function () {
-	     return true;
-	},
-	doCommand: function (aAddon)
-	{
-	    var cbs = Components. classes ["@xsms.nm.ru/custombuttons/cbservice;1" /* CB_SERVICE_CID */]. getService (Components. interfaces. cbICustomButtonsService /* CB_SERVICE_IID */);
-	    cbs. editButton (window, aAddon. buttonLink, null);
-	}
-    };
-    gViewController. commands. cmd_custombuttons_add = {
-	isEnabled: function () {
-	     return true;
-	},
-	doCommand: function (aAddon)
-	{
-	    var cbs = Components. classes ["@xsms.nm.ru/custombuttons/cbservice;1" /* CB_SERVICE_CID */]. getService (Components. interfaces. cbICustomButtonsService /* CB_SERVICE_IID */);
-	    cbs. editButton (window, "", null);
-	}
-    };
-    window. addEventListener ("ViewChanged", onViewChanged, false);
-    onViewChanged ();
-}
+     function init (aEvent) {
+	 gViewController. commands. cmd_custombuttons_edit = {
+	     isEnabled: function () {
+		 return true;
+	     },
+	     doCommand: function (aAddon) {
+		 var cbs = Components. classes ["@xsms.nm.ru/custombuttons/cbservice;1" /* CB_SERVICE_CID */].
+			   getService (Components. interfaces. cbICustomButtonsService /* CB_SERVICE_IID */);
+		 cbs. editButton (window, aAddon. buttonLink, null);
+	     }
+	 };
 
-function onViewChanged (aEvent)
-{
-    if ("addons://list/custombuttons" == gViewController. currentViewId)
-    {
-	document. documentElement. classList. add ("custombuttons");
-    }
-    else
-    {
-	document. documentElement. classList. remove ("custombuttons");
-    }
-};
+	 gViewController. commands. cmd_custombuttons_add = {
+	     isEnabled: function () {
+		 return true;
+	     },
 
-window. addEventListener ("load", init, false);
-}) ();
+	     doCommand: function (aAddon) {
+		 var cbs = Components. classes ["@xsms.nm.ru/custombuttons/cbservice;1" /* CB_SERVICE_CID */].
+			   getService (Components. interfaces. cbICustomButtonsService /* CB_SERVICE_IID */);
+		 cbs. editButton (window, "", null);
+	     }
+	 };
+
+	 window. addEventListener ("ViewChanged", onViewChanged, false);
+	 onViewChanged ();
+     }
+
+     function onViewChanged (aEvent) {
+	 if ("addons://list/custombuttons" == gViewController. currentViewId) {
+	     document. documentElement. classList. add ("custombuttons");
+	 } else {
+	     document. documentElement. classList. remove ("custombuttons");
+	 }
+     };
+
+     window. addEventListener ("load", init, false);
+ }) ();
