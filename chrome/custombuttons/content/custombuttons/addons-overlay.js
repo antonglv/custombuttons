640bb44406c8c188cf173abec4fd5a00cec19924
antonglv <none@none>
2013-02-06 23:35:54 +0700

    Reorganize code
    --HG--
    branch : bug24045

--- chrome/custombuttons/content/custombuttons/addons-overlay.js
+++ chrome/custombuttons/content/custombuttons/addons-overlay.js
@@ -1,69 +1,105 @@
 /* ****
- *
+ * addons-overlay.js
  */
-(function () {
-     Components. utils. import ("resource://gre/modules/AddonManager.jsm");
-     Components. utils. import ("resource://custombuttons-modules/addons4.js");
 
-     function init (aEvent) {
-	 gViewController. commands. cmd_custombuttons_edit = {
-	     isEnabled: function () {
-		 return "addons://list/custombuttons" == gViewController. currentViewId;
-	     },
-	     doCommand: function (aAddon) {
-		 var cbs = Components. classes ["@xsms.nm.ru/custombuttons/cbservice;1" /* CB_SERVICE_CID */].
-			   getService (Components. interfaces. cbICustomButtonsService /* CB_SERVICE_IID */);
-		 cbs. editButton (window, aAddon. buttonLink, null);
-	     }
-	 };
+Components. utils. import ("resource://gre/modules/AddonManager.jsm");
+Components. utils. import ("resource://custombuttons-modules/addons4.js");
 
-	 gViewController. commands. cmd_custombuttons_add = {
+var cbAddonManager = {
+    QueryInterface: function (iid) {
+	if (iid. equals (Components. interfaces. nsIObserver) ||
+	    iid. equals (Components. interfaces. nsIDOMEventListener) ||
+	    iid. equals (Components. interfaces. nsIWeakReference) ||
+	    iid. equals (Components. interfaces. nsISupports))
+	    return this;
+	return Components. results. NS_ERROR_NO_INTERFACE;
+    },
+
+    init: function () {
+	window. removeEventListener ("load", this, false);
+
+	gViewController. commands. cmd_custombuttons_edit = {
+	    isEnabled: function () {
+		return "addons://list/custombuttons" == gViewController. currentViewId;
+	    },
+
+	    doCommand: function (aAddon) {
+		var cbs = Components. classes ["@xsms.nm.ru/custombuttons/cbservice;1"].
+			  getService (Components. interfaces. cbICustomButtonsService);
+		cbs. editButton (window, aAddon. buttonLink, null);
+	    }
+	};
+
+	gViewController. commands. cmd_custombuttons_add = {
 	     isEnabled: function () {
 		 return true;
 	     },
 
 	     doCommand: function (aAddon) {
-		 var cbs = Components. classes ["@xsms.nm.ru/custombuttons/cbservice;1" /* CB_SERVICE_CID */].
-			   getService (Components. interfaces. cbICustomButtonsService /* CB_SERVICE_IID */);
+		 var cbs = Components. classes ["@xsms.nm.ru/custombuttons/cbservice;1"].
+			   getService (Components. interfaces. cbICustomButtonsService);
 		 cbs. editButton (window, "", null);
 	     }
 	 };
 
-	 var sortButton = document. getElementById ("custombuttons-sorting-name");
-	 sortButton. addEventListener ("command", changeSort, false);
+	 window. addEventListener ("ViewChanged", this, false);
+	 this. onViewChanged ();
+    },
 
-	 window. addEventListener ("ViewChanged", onViewChanged, false);
-	 onViewChanged ();
-     }
+    destroy: function (aEvent) {
+	window. removeEventListener ("ViewChanged", this, false);
+	window. removeEventListener ("unload", this, false);
+    },
 
-     function onViewChanged (aEvent) {
-	 if ("addons://list/custombuttons" == gViewController. currentViewId) {
-	     document. documentElement. classList. add ("custombuttons");
-	     applySort ();
-	 } else {
-	     document. documentElement. classList. remove ("custombuttons");
-	 }
-     }
+    onViewChanged: function (aEvent) {
+	if ("addons://list/custombuttons" == gViewController. currentViewId) {
+	    document. documentElement. classList. add ("custombuttons");
+	    this. sortButtons ();
+	} else {
+	    document. documentElement. classList. remove ("custombuttons");
+	}
+    },
 
-     function changeSort () {
-	 var sortButton = document. getElementById ("custombuttons-sorting-name");
-	 var checkState = (sortButton. getAttribute ("checkState") == "1")? "2": "1";
-	 sortButton. setAttribute ("checkState", checkState);
-	 applySort ();
-     }
+    changeSort: function () {
+	var sortButton = document. getElementById ("custombuttons-sorting-name");
+	var checkState = (sortButton. getAttribute ("checkState") == "1")? "2": "1";
+	sortButton. setAttribute ("checkState", checkState);
+	this. sortButtons ();
+    },
 
-     function applySort () {
-	 var sortButton = document. getElementById ("custombuttons-sorting-name");
-	 var ascending = ("1" != sortButton. getAttribute ("checkState"));
-	 var list = document. getElementById ("addon-list");
-	 var elements = Array. slice (list. childNodes, 0);
-	 sortElements (elements, ["name"], ascending);
-	 while (list. hasChildNodes ())
-	     list. removeChild (list. lastChild);
-	 elements. forEach (function (element) {
-	     list. appendChild (element);
-	 });
-     }
+    sortButtons: function () {
+	var sortButton = document. getElementById ("custombuttons-sorting-name");
+	var checkState = sortButton. getAttribute ("checkState");
+	this. applySort (["name"], checkState != "1");
+    },
 
-     window. addEventListener ("load", init, false);
-}) ();
+    applySort: function (sortFields, ascending) {
+	var list = document. getElementById ("addon-list");
+	var elements = Array. slice (list. childNodes, 0);
+	sortElements (elements, sortFields, ascending);
+	while (list. hasChildNodes ())
+	    list. removeChild (list. lastChild);
+	elements. forEach (function (element) {
+	    list. appendChild (element);
+	});
+    },
+
+    /* nsIDOMEventListener interface */
+    handleEvent: function (aEvent) {
+	switch (aEvent. type) {
+	    case "load":
+		this. init ();
+		break;
+	    case "unload":
+		this. destroy ();
+		break;
+	    case "ViewChanged":
+		this. onViewChanged (aEvent);
+		break;
+	    default:
+		break;
+	}
+    }
+};
+
+window. addEventListener ("load", cbAddonManager, false);
