ee1eef25fc896c82439708247fec3e75f5c4909e
antonglv <none@none>
2014-01-13 02:31:46 +0700

    Move all button initialization code into the separate file
    --HG--
    branch : experimental-0-0-5-7

---
+++ chrome/custombuttons/content/custombuttons/cbbuttonimpl0.js
@@ -0,0 +1,12 @@
+if (Components. lookupMethod)
+    Components. classes ["@mozilla.org/moz/jssubscript-loader;1"].
+	getService (Components. interfaces. mozIJSSubScriptLoader).
+	    loadSubScript ("chrome://custombuttons/content/cbbiNativeMethod.js", this);
+
+if (!this. _handlers)
+    Components. classes ["@mozilla.org/moz/jssubscript-loader;1"].
+	getService (Components. interfaces. mozIJSSubScriptLoader).
+	    loadSubScript ("chrome://custombuttons/content/cbbuttonimpl.js", this);
+
+if ("custombutton" in window)
+    custombutton. buttonConstructor (this);
