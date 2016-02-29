9bf5a5dcc03f39a39bb05b689f65e8d065874d0a
antonglv <none@none>
2016-02-29 16:26:02 +0700

    Add 'js-switch-indent-offset' emacs file variable

--- components/CBKeyMapComponent.js
+++ components/CBKeyMapComponent.js
@@ -1,4 +1,4 @@
-/* -*- mode: js; tab-width: 4; indent-tabs-mode: t; js-indent-level: 4 -*- */
+/* -*- mode: js; tab-width: 4; indent-tabs-mode: t; js-indent-level: 4; js-switch-indent-offset: 4 -*- */
 
 // ***** BEGIN LICENSE BLOCK *****
 // Version: MPL 1.1
