197370f08fff2d13a5039d4df11bb05e2fa2b887
antonglv <none@none>
2016-02-29 15:18:33 +0700

    Reformat code

--- components/CustomButtonProtocol.js
+++ components/CustomButtonProtocol.js
@@ -43,8 +43,7 @@
 var	  kSIMPLEURI_CONTRACTID = "@mozilla.org/network/simple-uri;1";
 var	  nsIURI = Components. interfaces. nsIURI;
 
-function CustombuttonProtocol (sProtocolName)
-{
+function CustombuttonProtocol (sProtocolName) {
 	this. scheme = sProtocolName;
 	this. protocolFlags = Components. interfaces. nsIProtocolHandler. URI_NORELATIVE |
 		Components. interfaces. nsIProtocolHandler. URI_NOAUTH |
@@ -75,56 +74,46 @@ CustombuttonProtocol. prototype = {
 
 	scheme: "custombutton",
 
-	allowPort: function (port, scheme)
-	{
+	allowPort: function (port, scheme) {
 		return false;
 	},
 
-	newURI: function (spec, charset, baseURI)
-	{
+	newURI: function (spec, charset, baseURI) {
 		var uri = Components. classes [kSIMPLEURI_CONTRACTID]. createInstance (nsIURI);
 		uri. spec = spec;
 		return uri;
 	},
 
-	get chromeProtocolHandler ()
-	{
+	get chromeProtocolHandler () {
 		var chromeProtocolHandler = Components. classes ["@mozilla.org/network/protocol;1?name=chrome"].
 			getService ();
 		chromeProtocolHandler = chromeProtocolHandler. QueryInterface (Components. interfaces. nsIProtocolHandler);
 		return chromeProtocolHandler;
 	},
 
-	get fakeOverlayURI ()
-	{
+	get fakeOverlayURI () {
 		var fakeOverlayURI = "chrome://custombuttons/content/buttonsoverlay.xul";
 		return this. chromeProtocolHandler. newURI (fakeOverlayURI, null, null);
 	},
 
-	fakeOverlayChannel: function ()
-	{
+	fakeOverlayChannel: function ()	{
 		return this. chromeProtocolHandler. newChannel (this. fakeOverlayURI);
 	},
 
 	sCbPrefix: "custombuttons://content/",
 
-	getChromePrincipal: function ()
-	{
+	getChromePrincipal: function ()	{
 		var ssm = Components. classes ["@mozilla.org/scriptsecuritymanager;1"]. getService (Components. interfaces. nsIScriptSecurityManager);
 		var res;
-		try
-		{
+		try	{
 			res = ssm. getCodebasePrincipal (this. fakeOverlayURI);
-		}
-		catch (e)
-		{
+		} catch (e)	{
 			res = this. fakeOverlayChannel (). owner;
 		}
 		return res;
 	},
 
-	getJSVersion: function ()
-	{
+	getJSVersion: function () {
 		var info = Components. classes ["@mozilla.org/xre/app-info;1"]. getService (Components. interfaces. nsIXULAppInfo);
 		var pv = info. platformVersion;
 		var oVC = Components. classes ["@mozilla.org/xpcom/version-comparator;1"]. createInstance (Components. interfaces. nsIVersionComparator);
@@ -134,8 +123,7 @@ CustombuttonProtocol. prototype = {
 		return "";
 	},
 
-	getXULTemplate: function ()
-	{
+	getXULTemplate: function ()	{
 		var ios = Components. classes ["@mozilla.org/network/io-service;1"]. getService (Components. interfaces. nsIIOService);
 		var xulchan = ios. newChannel ("chrome://custombuttons/content/tcbbutton.xul", null, null);
 		var instr = xulchan. open ();
@@ -146,8 +134,7 @@ CustombuttonProtocol. prototype = {
 		return doc;
 	},
 
-	pumpDocumentToPipe: function (doc, pipe)
-	{
+	pumpDocumentToPipe: function (doc, pipe) {
 		var bos = Components. classes ["@mozilla.org/binaryoutputstream;1"]. createInstance (Components. interfaces. nsIBinaryOutputStream);
 		bos. setOutputStream (pipe. outputStream);
 		var xs = Components. classes ["@mozilla.org/xmlextras/xmlserializer;1"]. createInstance (Components. interfaces. nsIDOMSerializer);
@@ -155,8 +142,7 @@ CustombuttonProtocol. prototype = {
 		bos. close ();
 	},
 
-	cbbuttonxulchannel: function (aURI)
-	{
+	cbbuttonxulchannel: function (aURI)	{
 		var pipe = Components. classes ["@mozilla.org/pipe;1"]. createInstance (Components. interfaces. nsIPipe);
 		pipe. init (true, true, 0, 0, null);
 		var doc = this. getXULTemplate ();
@@ -171,13 +157,11 @@ CustombuttonProtocol. prototype = {
 		return chan;
 	},
 
-	getCustomButtonsFile: function (aURI, sFileName)
-	{
+	getCustomButtonsFile: function (aURI, sFileName) {
 		var cbs = Components. classes ["@xsms.nm.ru/custombuttons/cbservice;1"]. getService (Components. interfaces. cbICustomButtonsService);
 		var dir = Components. classes ["@mozilla.org/file/directory_service;1"]. getService (Components. interfaces. nsIProperties). get ("ProfD", Components. interfaces. nsIFile); // get profile folder
 		dir. append ("custombuttons");
-		if (!dir. exists ())
-		{
+		if (!dir. exists ()) {
 			if (sFileName == "buttonsoverlay.xul")
 				cbs. makeOverlay ();
 			else
@@ -185,8 +169,7 @@ CustombuttonProtocol. prototype = {
 		}
 		var file = dir. clone ();
 		file. append (sFileName);
-		if (!file. exists ())
-		{
+		if (!file. exists ()) {
 			if (sFileName == "buttonsoverlay.xul")
 				cbs. makeOverlay ();
 			else
@@ -200,10 +183,8 @@ CustombuttonProtocol. prototype = {
 		return channel;
 	},
 
-	newChannel: function (aURI)
-	{
-		if (this. scheme == "custombuttons")
-		{
+	newChannel: function (aURI)	{
+		if (this. scheme == "custombuttons") {
 			var sFileName = aURI. spec. substring (this. sCbPrefix. length);
 			if (sFileName == "cbbutton.xul")
 				return this. cbbuttonxulchannel (aURI);
@@ -216,17 +197,14 @@ CustombuttonProtocol. prototype = {
 	}
 };
 
-function CustombuttonsProtocolClassFactory (sProtocolName)
-{
+function CustombuttonsProtocolClassFactory (sProtocolName) {
 	this. protocol = sProtocolName;
 	return this;
 }
-CustombuttonsProtocolClassFactory. prototype =
-	{
+CustombuttonsProtocolClassFactory. prototype = {
 		protocol: "",
 
-		createInstance: function (outer, iid)
-		{
+		createInstance: function (outer, iid) {
 			if (outer != null)
 				throw Components. results. NS_ERROR_NO_AGGREGATION;
 			if (!iid. equals (Components. interfaces. nsIProtocolHandler) &&
@@ -236,51 +214,44 @@ CustombuttonsProtocolClassFactory. prototype =
 		}
 	};
 
-var Module =
-	{
-		CLSID: [Components. ID ("{78D452B8-2CE8-4a7b-8A59-DA3C0960DAE7}"),
-				Components. ID ("{1c796f9e-9a22-4604-84e4-fa7c4b8d80a4}")],
-		ContractID: ["@mozilla.org/network/protocol;1?name=custombutton",
-					 "@mozilla.org/network/protocol;1?name=custombuttons"],
-		ComponentName: ["Custombutton Protocol", "Custombuttons Extension Protocol"],
-		protocolName: ["custombutton", "custombuttons"],
+var Module = {
+	CLSID: [Components. ID ("{78D452B8-2CE8-4a7b-8A59-DA3C0960DAE7}"),
+			Components. ID ("{1c796f9e-9a22-4604-84e4-fa7c4b8d80a4}")],
+	ContractID: ["@mozilla.org/network/protocol;1?name=custombutton",
+				 "@mozilla.org/network/protocol;1?name=custombuttons"],
+	ComponentName: ["Custombutton Protocol", "Custombuttons Extension Protocol"],
+	protocolName: ["custombutton", "custombuttons"],
 
-		canUnload: function (componentManager)
-		{
-			return true;
-		},
+	canUnload: function (componentManager) {
+		return true;
+	},
 
-		getClassObject: function (componentManager, cid, iid)
-		{
-			if (!cid. equals (this. CLSID [0]) &&
-				!cid. equals (this. CLSID [1]))
-				throw Components. results. NS_ERROR_NO_INTERFACE;
-			if (!iid. equals (Components. interfaces. nsIFactory))
-				throw Components. results. NS_ERROR_NOT_IMPLEMENTED;
-			var protocol;
-			for (var i = 0; i < this. CLSID. length; i++)
-			{
-				if (cid. equals (this. CLSID [i]))
-				{
-					protocol = this. protocolName [i];
-					break;
-				}
+	getClassObject: function (componentManager, cid, iid) {
+		if (!cid. equals (this. CLSID [0]) &&
+			!cid. equals (this. CLSID [1]))
+			throw Components. results. NS_ERROR_NO_INTERFACE;
+		if (!iid. equals (Components. interfaces. nsIFactory))
+			throw Components. results. NS_ERROR_NOT_IMPLEMENTED;
+		var protocol;
+		for (var i = 0; i < this. CLSID. length; i++) {
+			if (cid. equals (this. CLSID [i])) {
+				protocol = this. protocolName [i];
+				break;
 			}
-			return new CustombuttonsProtocolClassFactory (protocol);
-		},
+		}
+		return new CustombuttonsProtocolClassFactory (protocol);
+	},
 
-		FIRST_TIME: true,
+	FIRST_TIME: true,
 
-		registerSelf: function (componentManager, fileSpec, location, type)
-		{
-			if (this. FIRST_TIME)
-				this. FIRST_TIME = false;
-			else
-				throw Components. results. NS_ERROR_FACTORY_REGISTER_AGAIN;
-			componentManager = componentManager. QueryInterface (Components. interfaces. nsIComponentRegistrar);
-			for (var i = 0; i < this. CLSID. length; i++)
-				componentManager. registerFactoryLocation
-			(
+	registerSelf: function (componentManager, fileSpec, location, type)	{
+		if (this. FIRST_TIME)
+			this. FIRST_TIME = false;
+		else
+			throw Components. results. NS_ERROR_FACTORY_REGISTER_AGAIN;
+		componentManager = componentManager. QueryInterface (Components. interfaces. nsIComponentRegistrar);
+		for (var i = 0; i < this. CLSID. length; i++)
+			componentManager. registerFactoryLocation (
 				this. CLSID [i],
 				this. ComponentName [i],
 				this. ContractID [i],
@@ -288,13 +259,12 @@ var Module =
 				location,
 				type
 			);
-		},
+	},
 
-		unregisterSelf: function (componentManager, location, loaderStr) {}
-	};
+	unregisterSelf: function (componentManager, location, loaderStr) {}
+};
 
-function NSGetFactory (cid)
-{
+function NSGetFactory (cid) {
 	var protocol;
 	if (cid. equals (Components. ID ("{78D452B8-2CE8-4a7b-8A59-DA3C0960DAE7}")))
 		protocol = "custombutton";
